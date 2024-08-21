272eef885b5fffbbb77f1cb2c42fecc1255fc419
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-08-21 18:04:47 +0500

    Add Search with debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,16 +2,17 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term) => {
     // console.log(term); //$$$ it will show up in Dev tool console and not in VScode terminal as client comp
     const params = new URLSearchParams(searchParams);
+    console.log(`Searching... ${term}`);
 
     if (term) {
       params.set('query', term);
@@ -19,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
